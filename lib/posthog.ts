9a7ad7fe9fa4f1c2f537d9abf336f89f8f519cd6
Kumar Gaurav Singh <kumargauravsingh@Kumars-MacBook-Pro.local>
2025-06-23 08:31:12 +0530

    fix: posthog integration

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,15 +1,24 @@
 import posthog from 'posthog-js';
 
 export function initPostHog() {
-  if (typeof window !== 'undefined' && !posthog.__initialized) {
+//   if (typeof window !== 'undefined' && !posthog.__initialized) {
+//     posthog.init(
+//       process.env.PTK as string,
+//       {
+//         api_host: process.env.PTH,
+//         loaded: (ph) => { ph.__initialized = true; }
+//       }
+//     );
+//   }
+if (typeof window !== 'undefined' && !posthog.__loaded) {
     posthog.init(
       process.env.PTK as string,
       {
-        api_host: process.env.PTH,
-        loaded: (ph) => { ph.__initialized = true; }
+        api_host: process.env.PTH || 'https://app.posthog.com',
+        loaded: (ph) => { console.log("Posthog Loaded") }
       }
     );
-  }
+  }  
 }
 
 export default posthog;
